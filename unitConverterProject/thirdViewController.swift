ff76c1410b06f8b5317aedf5aacafa4de25c4555
maxineludington <maxine.ludington@gmail.com>
2017-07-06 19:16:06 -0500

    Set up storyboard and complete code for fahrenheit and celsius

---
+++ unitConverterProject/thirdViewController.swift
@@ -0,0 +1,43 @@
+//
+//  thirdViewController.swift
+//  unitConverterProject
+//
+//  Created by Maxine Ludington on 7/6/17.
+//  Copyright © 2017 Maxine Ludington. All rights reserved.
+//
+
+import UIKit
+
+class thirdViewController: UIViewController {
+
+    @IBOutlet weak var fahrenheitTextField: UITextField!
+    @IBOutlet weak var celsiusTextField: UITextField!
+    @IBOutlet weak var fInCLabel: UILabel!
+    @IBOutlet weak var cInFLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    func convertToCelsius(){
+        if let amount = Double(fahrenheitTextField.text!){
+            let celsius = round((5.0 / 9.0 * (Double(fahrenheitTextField.text!)! - 32.0)))
+            
+            fInCLabel.text = String(celsius) + " °C"
+    }
+    }
+    
+    func convertToFahrenheit(){
+        if let amount = Double(celsiusTextField.text!){
+            let fahrenheit = round((9.0 / 5.0 * (Double(celsiusTextField.text!)! + 32.0)))
+            cInFLabel.text = String(fahrenheit) + " °F"
+    }
+    }
+    
+    @IBAction func fInCButtonPressed(_ sender: Any) {
+        convertToCelsius()
+    }
+    @IBAction func cInFButtonPressed(_ sender: Any) {
+        convertToFahrenheit()
+    }
+}

5aa7d12d0b35e02bd13674d253289eff8d0d317b
maxineludington <maxine.ludington@gmail.com>
2017-07-13 10:04:23 -0500

    Trying again

--- unitConverterProject/ViewController.swift
+++ unitConverterProject/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var byKat: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,11 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let dvc = segue.destination as! thirdViewController
+        dvc.byKat = byKat.text!
     }
 
 
--- unitConverterProject/thirdViewController.swift
+++ unitConverterProject/thirdViewController.swift
@@ -9,28 +9,32 @@
 import UIKit
 
 class thirdViewController: UIViewController {
-
+    
+    @IBOutlet weak var myNameLabel: UILabel!
     @IBOutlet weak var fahrenheitTextField: UITextField!
     @IBOutlet weak var celsiusTextField: UITextField!
     @IBOutlet weak var fInCLabel: UILabel!
     @IBOutlet weak var cInFLabel: UILabel!
     
+    var byKat = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        myNameLabel.text = "\(byKat)"
     }
-
+    
     func convertToCelsius(){
         if let amount = Double(fahrenheitTextField.text!){
             let celsius = round((5.0 / 9.0 * (Double(fahrenheitTextField.text!)! - 32.0)))
             fInCLabel.text = String(celsius) + " °C"
-    }
+        }
     }
     
     func convertToFahrenheit(){
         if let amount = Double(celsiusTextField.text!){
-            let fahrenheit = round((9.0 / 5.0 * (Double(celsiusTextField.text!)! + 32.0)))
+            let fahrenheit = round(Double(celsiusTextField.text!)! * 9.0 / 5.0  + 32.0)
             cInFLabel.text = String(fahrenheit) + " °F"
-    }
+        }
     }
     
     @IBAction func fInCButtonPressed(_ sender: Any) {
